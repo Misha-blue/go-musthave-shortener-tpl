87352b436ac7dfc04720701b914930b075fa8bb7
Misha-blue <vorosviktoriya@yandex.com>
2021-11-19 22:07:07 +0300

    added some logic to test

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+var urls = []string{}
+
+var handeledURL = map[string]string{}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func getShortenUrl(s []string, e string) string {
+	for i, a := range s {
+		if a == e {
+			return fmt.Sprintf("%d", i)
+		}
+	}
+	return ""
+}
+
+func HandleUrlRequest(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		id := string(r.URL.Path)[1:]
+		if handeledURL[id] != "" {
+			w.Header().Add("Content-Type", "application/json")
+			w.Header().Set("Location", handeledURL[id])
+
+		} else {
+			http.Error(w, "No such id", http.StatusBadRequest)
+		}
+	case http.MethodPost:
+		body, error := io.ReadAll(r.Body)
+		if error != nil {
+			http.Error(w, error.Error(), 400)
+			return
+		}
+		url := string(body)
+		if !contains(urls, url) {
+			urls = append(urls, url)
+			handeledURL[getShortenUrl(urls, url)] = url
+		}
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(getShortenUrl(urls, url)))
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	// маршрутизация запросов обработчику
+	http.HandleFunc("/", HandleUrlRequest)
+
+	// запуск сервера с адресом localhost, порт 8080
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
