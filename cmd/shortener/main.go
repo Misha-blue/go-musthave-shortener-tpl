2e275cd3e849692e4a5f5345667b3ef526dd8929
Misha-blue <vorosviktoriya@yandex.com>
2021-11-30 22:01:01 +0300

    moved repo/handler into main

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -23,11 +23,8 @@ func main() {
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	go func() {
-		oscall := <-done
-		log.Printf("system call:%+v", oscall)
-		cancel()
-	}()
+	oscall := <-done
+	log.Printf("system call:%+v", oscall)
 
 	if err := server.Run(ctx); err != nil {
 		log.Printf("failed to run server:+%v\n", err)
